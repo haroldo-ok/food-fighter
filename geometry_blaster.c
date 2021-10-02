92e071d72e9dae3a507f66eb264721ab5efc5e2c
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-10-02 08:55:09 -0300

    Test enemy formation.

--- geometry_blaster.c
+++ geometry_blaster.c
@@ -6,16 +6,20 @@
 #include "actor.h"
 #include "data.h"
 
-#define PLAYER_SPEED 2
-#define PLAYER_SHOT_SPEED 4
+#define PLAYER_SPEED (2)
+#define PLAYER_SHOT_SPEED (4)
 #define PLAYER_TOP (16)
 #define PLAYER_LEFT (8)
 #define PLAYER_RIGHT (256 - 16)
 #define PLAYER_BOTTOM (SCREEN_H - 24)
 
+#define MAX_ENEMIES_X (3)
+#define MAX_ENEMIES_Y (3)
+
 actor player;
 actor shot;
-actor enemy;
+
+actor enemies[MAX_ENEMIES_Y][MAX_ENEMIES_X];
 
 void load_standard_palettes() {
 	SMS_loadBGPalette(sprites_palette_bin);
@@ -48,6 +52,48 @@ void handle_shot_movement() {
 	if (shot.y < -8) shot.active = 0;
 }
 
+void init_enemies() {
+	static char i, j;
+	static int x, y;
+	static actor *enemy;
+
+	for (i = 0, y = 0; i != MAX_ENEMIES_Y; i++, y += 32) {
+		enemy = enemies[i];
+		for (j = 0, x = i & 1 ? 32 : 0; j != MAX_ENEMIES_X; j++, x += (256 / 3)) {
+			init_actor(enemy, x, i << 5, 2, 1, 64, 6);
+			enemy++;
+		}
+	}
+}
+
+void draw_enemies() {
+	static char i, j;
+	static actor *enemy;
+
+	for (i = 0; i != MAX_ENEMIES_Y; i++) {
+		enemy = enemies[i];
+		for (j = 0; j != MAX_ENEMIES_X; j++) {
+			draw_actor(enemy);
+			enemy++;
+		}
+	}
+}
+
+void handle_enemies_movement() {
+	static char i, j;
+	static actor *enemy;
+
+	for (i = 0; i != MAX_ENEMIES_Y; i++) {
+		enemy = enemies[i];
+		for (j = 0; j != MAX_ENEMIES_X; j++) {
+			enemy->x++;
+			if (enemy->x > 255) enemy->x -= 255;
+
+			enemy++;
+		}
+	}
+}
+
 void main() {
 	SMS_useFirstHalfTilesforSprites(1);
 	SMS_setSpriteMode(SPRITEMODE_TALL);
@@ -61,19 +107,21 @@ void main() {
 	
 	init_actor(&player, 120, PLAYER_BOTTOM, 2, 1, 2, 1);
 	init_actor(&shot, 120, PLAYER_BOTTOM - 8, 2, 1, 6, 1);
-	init_actor(&enemy, 120, 32, 2, 1, 64, 6);
+
+	init_enemies();
 	
 	shot.active = 0;
 
 	while (1) {
 		handle_player_input();
 		handle_shot_movement();
+		handle_enemies_movement();
 		
 		SMS_initSprites();
 
 		draw_actor(&player);
 		draw_actor(&shot);
-		draw_actor(&enemy);
+		draw_enemies();
 		
 		SMS_finalizeSprites();
 		SMS_waitForVBlank();
